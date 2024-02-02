e2de71b633e179c00fca76361b3b509824fb2d30
Ivan Semikin <i.semikin.94@icloud.com>
2024-02-02 19:34:00 +0100

    Logic of change colors

--- CustomGRBViewHW3/ViewController.swift
+++ CustomGRBViewHW3/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var rgbView: UIView!
+    
+    @IBOutlet var redLevelLabel: UILabel!
+    @IBOutlet var greenLevelLabel: UILabel!
+    @IBOutlet var blueLevelLabel: UILabel!
+    
+    @IBOutlet var setRedLevelSlider: UISlider!
+    @IBOutlet var setGreenLevelSlider: UISlider!
+    @IBOutlet var setBlueLevelSlider: UISlider!
+    
+    private var rgbLevels: UIColor {
+        return UIColor(
+            red: CGFloat(setRedLevelSlider.value),
+            green: CGFloat(setGreenLevelSlider.value),
+            blue: CGFloat(setBlueLevelSlider.value),
+            alpha: 1.0
+        )
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        rgbView.layer.cornerRadius = 15
+        rgbView.backgroundColor = rgbLevels
     }
 
-
+    @IBAction func redSliderAction() {
+        redLevelLabel.text = String(format: "%.2f", setRedLevelSlider.value)
+        rgbView.backgroundColor = rgbLevels
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLevelLabel.text = String(format: "%.2f", setGreenLevelSlider.value)
+        rgbView.backgroundColor = rgbLevels
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLevelLabel.text = String(format: "%.2f", setBlueLevelSlider.value)
+        rgbView.backgroundColor = rgbLevels
+    }
 }
 
