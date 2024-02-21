aa7446011ff791272fd68f6e33275d01d832d52b
Ivan Semikin <i.semikin.94@icloud.com>
2024-02-21 18:59:17 +0100

    Create MainViewController and write logics

--- CustomGRBViewHW3/MainViewController.swift
+++ CustomGRBViewHW3/MainViewController.swift
@@ -7,23 +7,33 @@
 
 import UIKit
 
+protocol SettingsViewControllerDelegate: AnyObject {
+    func setRGBBackgroundView(
+        withRed red: CGFloat,
+        green: CGFloat,
+        blue: CGFloat,
+        alpha: CGFloat
+    )
+}
+
 class MainViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let settingsVC = segue.destination as? SettingsViewController
+        
+        settingsVC?.backgroundColor = view.backgroundColor
+        settingsVC?.delegate = self
     }
-    */
+}
 
+// MARK: - SettingsViewControllerDelegate
+extension MainViewController: SettingsViewControllerDelegate {
+    func setRGBBackgroundView(withRed red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        view.backgroundColor = UIColor(
+            red: red,
+            green: green,
+            blue: blue,
+            alpha: alpha
+        )
+    }
 }
